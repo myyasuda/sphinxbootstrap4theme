6d0953749061c693e9eed4742e31eea0800cba6a
masahiko_yasuda <mshkysd0821@gmail.com>
2016-05-16 13:14:25 +0900

    version0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'sphinxbootstrap4theme',
-    version = '0.1.0',
+    version = '0.1.1',
     author = 'Masahiko Yasuda',
     author_email= 'myasuda@uchida.co.jp',
     url="https://github.com/myyasuda/sphinxbootstrap4theme",
